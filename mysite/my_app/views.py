dd40d0fccb85639ae1e23055ffbe46b3afaa5066
Sergey Trunov <sergeytru15@gmail.com>
2022-08-03 23:55:34 +1000

    Init django.

---
+++ mysite/my_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'my_app/index.html',
+                  context)
+
+
+def search(request):
+    context = {}
+    return render(request, 'my_app/search.html', context)
+
+
+def auth(request):
+    context = {}
+    return render(request, 'my_app/authentication.html', context)
