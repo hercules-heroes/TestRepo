a8be3972a570cc462a9088fb7ff70ef8f1cfb103
SysolyatinPavel <sysolyatinpavel84@yandex.ru>
2022-08-11 16:47:42 +0300

    add new class Person

---
+++ user.py
@@ -0,0 +1,21 @@
+class Person:
+    def __init__(self,
+                 name: str,
+                 surname: str,
+                 sex: str,
+                 city: str) -> None:
+        self.name = name
+        self.surname = surname
+        self.sex = sex
+        self.city = city
+
+    def display_info(self) -> str:
+        """Вывести сообщение о пользователе"""
+        message = (f"Фамилия: {self.surname}\nИмя: {self.name}\n"
+                   f"Пол: {self.sex}\nГород: {self.city}")
+        return message
+
+
+user = Person("Pavel", "Sysolyatin", "Male", "Kirov")
+info = user.display_info()
+print(info)
