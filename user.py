6300c88d27c37fd0e96f6e4fd03ba7ab35f7e2a9
Trunov Sergey <sergeytru15@gmail.com>
2022-08-14 16:23:17 +0900

    Update class Person

--- user.py
+++ user.py
@@ -5,12 +5,14 @@ class Person:
                  sex: str,
                  city: str) -> None:
         self.name = name
-        self.surname = surname
-        self.sex = sex
+        self.lastname = surname
+        self.gender = sex
         self.city = city
 
     def display_info(self) -> str:
         """Вывести сообщение о пользователе"""
-        message = (f"Фамилия: {self.surname}\nИмя: {self.name}\n"
-                   f"Пол: {self.sex}\nГород: {self.city}")
+        message = (f"Фамилия: {self.lastname}\n"
+                   f"Имя: {self.name}\n"
+                   f"Пол: {self.gender}\n"
+                   f"Город: {self.city}")
         return message
